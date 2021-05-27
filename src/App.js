0e191c8ae295dd5884a8158fab65b5a35571163c
appleramos <apple.r08@gmail.com>
2021-05-27 13:11:03 +0800

    Add player data columns

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import 'antd/dist/antd.css'
-import React, { useState, } from 'react'
+import React, { useState, useEffect, } from 'react'
 import { filter } from 'lodash'
+import axios from 'axios'
+import numeral from 'numeral'
+import moment from 'moment'
 
 import { Table, Popconfirm, Button, message, } from 'antd'
 import { DeleteOutlined, QuestionCircleOutlined, } from '@ant-design/icons';
@@ -12,6 +15,40 @@ import DataRow from './components/DataRow';
 function App() {
   const storagePlayers = JSON.parse(window.localStorage.getItem('players')) || { value: [] }
   const [ players, setPlayers ] = useState(storagePlayers)
+  const [ playersData, setPlayersData ] = useState([])
+  const [ tableLoading, setTableLoading ] = useState(true)
+
+  useEffect(() => {
+    async function fetchData(player) {
+      const res = await axios(`https://lunacia.skymavis.com/game-api/clients/${player.address}/items/1`);
+      if (res.data) {
+        console.log(playersData)
+        let newPlayersData = playersData
+        const {
+          client_id,
+          total,
+          claimable_total,
+          last_claimed_item_at,
+        } = res.data
+        newPlayersData.push({
+          id: client_id,
+          total: total || 0,
+          claimable: claimable_total || 0,
+          lockedSlp: total - claimable_total,
+          lastClaimedAt: last_claimed_item_at,
+          dailyAvg: getDailyAvg(last_claimed_item_at, total),
+          nextClaimDate: getNextClaimDate(last_claimed_item_at)
+        })
+        setPlayersData(newPlayersData)
+        if (newPlayersData.length === players.value.length) {
+          setTableLoading(false)
+        }
+      }
+    }
+    players.value.forEach((player) => {
+      fetchData(player)
+    })
+  }, ['players'])
 
   const renderAddress = (text) => {
     const firstDigits = text.substring(0, 6)
@@ -34,30 +71,99 @@ function App() {
       </Popconfirm>
     )
   }
+
+  const getDailyAvg = (lastClaimedAt, total) => {
+    const lastClaimDate = moment(new Date(lastClaimedAt * 1000))
+    const dateNow = moment()
+    const dateDiff = dateNow.diff(lastClaimDate, 'days')
+
+    return total/dateDiff
+  }
+
+  const getNextClaimDate = (lastClaimedAt) => {
+    const lastClaimDate = moment(new Date(lastClaimedAt * 1000))
+    return lastClaimDate.add(14, 'days').format('LLL')
+  }
+
+  const getFromPlayersData = (ethAddress, dataKey, format) => {
+    const playerData = filter(playersData, player => player.id === ethAddress)
+    if (playerData.length > 0) {
+      const data = playerData[0][dataKey]
+      switch (format) {
+        case 'number':
+          return numeral(data).format('0,0')
+        case 'date':
+          return moment(new Date(data * 1000)).format('LLL')
+        case 'decimal':
+          return numeral(data).format('0.0')
+        default:
+          return data;
+      }
+    }
+    return '-'
+  }
+
   const columns = [
     {
-      title: 'Action',
+      title: '',
       render: renderButton,
-      width: '10%'
+      width: 5,
+      // fixed: 'left',
     },
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
-      width: '30%',
+      width: 10,
+      // fixed: 'left',
     },
     {
-      title: 'Player Data',
-      dataIndex: 'slp',
-      key: 'slp',
-      width: '40%',
-      render: (_, record) => <DataRow address={ record.address }/>
+      title: 'Claimable SLP',
+      dataIndex: 'address',
+      key: 'address',
+      width: 50,
+      render: (_, record) => <span>{getFromPlayersData(record.address, 'claimable', 'number')}</span>
+    },
+    {
+      title: 'Locked SLP',
+      dataIndex: 'address',
+      key: 'address',
+      width: 50,
+      render: (_, record) => <span>{getFromPlayersData(record.address, 'lockedSlp', 'number')}</span>
+    },
+    {
+      title: 'Total SLP',
+      dataIndex: 'address',
+      key: 'address',
+      width: 50,
+      render: (_, record) => <span>{getFromPlayersData(record.address, 'total', 'number')}</span>
+    },
+    {
+      title: 'Daily Avg',
+      dataIndex: 'address',
+      key: 'address',
+      width: 50,
+      render: (_, record) => <span>{getFromPlayersData(record.address, 'dailyAvg', 'decimal')}</span>
+    },
+    {
+      title: 'Last Claim Date',
+      dataIndex: 'address',
+      key: 'address',
+      width: 150,
+      render: (_, record) => <span>{getFromPlayersData(record.address, 'lastClaimedAt', 'date')}</span>
+    },
+    {
+      title: 'Next Claim Date',
+      dataIndex: 'address',
+      key: 'address',
+      width: 150,
+      render: (_, record) => <span>{getFromPlayersData(record.address, 'nextClaimDate', 'none')}</span>
     },
     {
       title: 'Address',
       dataIndex: 'address',
       key: 'address',
-      width: '20%',
+      width: 50,
       render: renderAddress
     },
   ]
@@ -93,8 +199,10 @@ function App() {
       </div>
       <UserInput onSubmit={ handleSubmit }/>
       <Table 
+        loading={ tableLoading }
         dataSource={ players.value }
         columns={ columns }
+        scroll={{ x: 1300 }}
       />
     </div>
   );
